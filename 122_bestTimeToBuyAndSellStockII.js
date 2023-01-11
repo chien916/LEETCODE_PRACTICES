247017357e42e82d7c97b96bec686d5ae949a61f
chien916 <chien916@outlook.com>
2023-01-10 21:03:50 -0500

    .

--- 122_bestTimeToBuyAndSellStockII.js
+++ 122_bestTimeToBuyAndSellStockII.js
@@ -2,6 +2,13 @@
  * @param {number[]} prices
  * @return {number}
  */
-var maxProfit = function(prices) {
-    
+var maxProfit = function (prices) {
+    if (prices.length === 1) return 0;
+    let maxProf_n = 0;
+    for (let ind_n = 0; ind_n < prices.length; ++ind_n) {
+        if (ind_n === 0) continue;
+        let currDiff_n = prices[ind_n] - prices[ind_n - 1];
+        if (currDiff_n > 0) { maxProf_n += currDiff_n; }
+    }
+    return maxProf_n;
 };
