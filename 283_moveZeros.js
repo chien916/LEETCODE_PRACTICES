43fd69a9212e56a36c0184de5d8f9d95c949cf4e
chien916 <chien916@outlook.com>
2023-05-11 14:44:32 +0800

    2023-05-11 14:44:32Z

--- 283_moveZeros.js
+++ 283_moveZeros.js
@@ -3,19 +3,19 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 var moveZeroes = function (nums) {
-	let [currLeftInd_n, currRightInd_n] = [0, nums.length - 1];
-	while (currLeftInd_n < currRightInd_n) {
-		if (nums[currLeftInd_n] === 0) {
-			[nums[currLeftInd_n], nums[currRightInd_n]] = [nums[currRightInd_n], nums[currLeftInd_n]];
-			--currRightInd_n;
+	if (nums.length === 1) return;
+	for (let l_i = 0; l_i < nums.length; ++l_i) {
+		// 0 1 2 3 length=4 lastInd=3 1
+		if (nums[l_i] === 0) {
+			//till now, l points to a zero, lets find a non-zero
+			for (let r_i = l_i; r_i < nums.length; ++r_i) {
+				if (nums[r_i] !== 0) {
+					let helper_n = nums[l_i];
+					nums[l_i] = nums[r_i];
+					nums[r_i] = helper_n;
+					break;
+				}
+			}
 		}
-		++currLeftInd_n;
-	}
-	currLeftInd_n = 0;
-	while (currLeftInd_n < currRightInd_n) {
-		[nums[currLeftInd_n], nums[currRightInd_n]] = [nums[currRightInd_n], nums[currLeftInd_n]];
-		--currRightInd_n;
-		++currLeftInd_n;
 	}
 };
-moveZeroes([0,1,0,3,12])
